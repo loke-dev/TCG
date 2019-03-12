8c3902c58419266cef68c093797b47208fe6e7b0
LokeCarlsson <carlssonloke@gmail.com>
2019-03-12 21:35:33 +0100

    MVP

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,26 +1,13 @@
 module.exports = {
-  /*
-  ** Build configuration
-  */
   build: {},
-  /*
-  ** Headers
-  ** Common headers are already provided by @nuxtjs/pwa preset
-  */
   head: {},
-  /*
-  ** Customize the progress-bar color
-  */
+  server: {
+    port: 8080
+  },
   loading: { color: '#3B8070' },
-  /*
-  ** Customize app manifest
-  */
   manifest: {
     theme_color: '#3B8070'
   },
-  /*
-  ** Modules
-  */
   modules: [
     '@nuxtjs/pwa'
   ]
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,26 @@
 <template>
   <section class="container">
-    <div>
+    <div class="header">
       <h1 class="title">
         TCG
       </h1>
       <h2 class="subtitle">
         Team Color Generator
       </h2>
+    </div>
+    <div class="content">
+      <div class="colors-wrapper">
+        <div class="team1">
+          <span>{{colors[0].name}}</span>
+          <span>{{colors[1].name}}</span>
+        </div>
+        <span class="versus">vs</span>
+        <div class="team2">
+          <span>{{colors[2].name}}</span>
+          <span>{{colors[3].name}}</span>
+        </div>
+      </div>
+      <button @click="randomize" class="button--randomize">Randomize</button>
     </div>
     <div class="links">
       <a href="https://lokecarlsson.se" target="_blank" class="button--green" rel="noopener">Author</a>
@@ -18,19 +32,43 @@
 <script>
 
   export default {
-    components: {
-      
-    },
     data () {
       return {
+        colors: this.shuffle()
       }
     },
     mounted () {
     },
     methods: {
+      randomize() {
+        this.colors = this.shuffle(this.colors)
+      },
+      shuffle() {
+        const startColors = [
+          {
+            name: 'Blue',
+            id: 'blue'
+          },
+          {
+            name: 'Red',
+            id: 'red'
+          },
+          {
+            name: 'Purple',
+            id: 'purple'
+          },
+          {
+            name: 'Yellow',
+            id: 'yellow'
+          }
+        ]
+
+        return startColors
+          .map(a => [Math.random(), a])
+          .sort((a, b) => a[0] - b[0])
+          .map(a => a[1]);
+      }
     },
-    destroyed () {
-    }
   }
 </script>
 
@@ -38,6 +76,29 @@
 .container {
   display: grid;
   min-height: 100vh;
+}
+
+.content {
+  margin: auto;
+  display: grid;
+}
+
+.colors-wrapper {
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+
+.colors-wrapper p {
+  margin: 20px;
+}
+
+.versus {
+  padding: 30px;
+}
+
+.team1, .team2 {
+  display: grid;
 }
 
 .links {
@@ -73,5 +134,16 @@
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+.button--randomize {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #b718df;
+  color: #b718df;
+  text-decoration: none;
+  padding: 10px 30px;
+  cursor: pointer;
+  margin: auto;
 }
 </style>
