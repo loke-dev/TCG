9ec0a3f058b1ac04caf333459835d3bf72139a77
LokeCarlsson <deleted@example.com>
2019-04-07 21:27:41 +0200

    Add names to manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,9 @@ module.exports = {
   loading: { color: '#232323' },
   manifest: {
     theme_color: '#26a69a',
-    background_color: '#232323'
+    background_color: '#232323',
+    name: 'Team Color Generator',
+    short_name: 'TCG'
   },
   modules: [
     '@nuxtjs/pwa',
