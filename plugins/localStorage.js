8ae904f7a9868fae4573aea4582fe6a368a15ef9
LokeCarlsson <carlssonloke@gmail.com>
2019-04-08 22:24:48 +0200

    Add and use isClient

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -23,12 +23,12 @@ import createPersistedState from 'vuex-persistedstate'
 import * as Cookies from 'js-cookie'
 import cookie from 'cookie'
 
-export default ({store, req, isDev}) => {
+export default ({store, req, isDev, isClient}) => {
   createPersistedState({
     vuex: 'vuex',
     paths: ['colors'],
     storage: {
-      getItem: (key) => process.client ? Cookies.getJSON(key) : cookie.parse(req.headers.cookie || '')[key],
+      getItem: (key) => isClient ? Cookies.getJSON(key) : cookie.parse(req.headers.cookie || '')[key],
       setItem: (key, value) => Cookies.set(key, value, { expires: 365, secure: !isDev }),
       removeItem: (key) => Cookies.remove(key)
     }
