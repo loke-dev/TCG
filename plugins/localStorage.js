d92ecbd1f5591e88e15b20005df38c4a86340e25
LokeCarlsson <carlssonloke@gmail.com>
2019-04-08 22:25:41 +0200

    Remove dead code

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -1,24 +1,3 @@
-// import createPersistedState from 'vuex-persistedstate'
-// import * as Cookies from 'js-cookie'
-
-// export default ({store, isHMR, isClient, isServer}) => {
-//   console.log('plugin')
-//   if (isHMR) return
-//   console.log('after HMR')
-//   if (isClient) {
-//     console.log('is client')
-//     window.onNuxtReady((nuxt) => {
-//       createPersistedState({
-//         storage: {
-//           getItem: key => Cookies.get(key),
-//           setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: true }),
-//           removeItem: key => Cookies.remove(key)
-//         }
-//       })(store)
-//     })
-//   }
-// }
-
 import createPersistedState from 'vuex-persistedstate'
 import * as Cookies from 'js-cookie'
 import cookie from 'cookie'
