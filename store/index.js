e3b39114240a671bccaf2726925f79d9cd833b74
LokeCarlsson <carlssonloke@gmail.com>
2019-04-08 21:42:49 +0200

    Add persistance

---
+++ store/index.js
@@ -0,0 +1,46 @@
+const getDefaultState = () => {
+  return {
+    colors: [
+      {
+        name: 'Blue',
+        id: 'blue'
+      },
+      {
+        name: 'Red',
+        id: 'red'
+      },
+      {
+        name: 'Purple',
+        id: 'purple'
+      },
+      {
+        name: 'Yellow',
+        id: 'yellow'
+      }
+    ]
+  }
+}
+
+const state = getDefaultState()
+
+const actions = {
+  shuffle ({ commit }) {
+    commit('shuffle')
+  }
+}
+
+const mutations = {
+  shuffle (state) {
+    state.colors = state.colors
+      .map(a => [Math.random(), a])
+      .sort((a, b) => a[0] - b[0])
+      .map(a => a[1])
+  }
+}
+
+export default {
+  state,
+  getters: {},
+  actions,
+  mutations
+}
