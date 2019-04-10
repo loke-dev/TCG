820106611d3d26e0fda7bdf07e7daaf8bd98f31e
LokeCarlsson <deleted@example.com>
2019-04-10 19:54:46 +0200

    Stuff

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,9 +1,12 @@
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import * as Cookies from 'js-cookie'
+import { register } from 'register-service-worker'
 
 export default function (Vue, options, {
-  appOptions
+  appOptions,
+  isClient,
+  head
 }) {
   Vue.use(Vuex)
 
@@ -56,5 +59,36 @@ export default function (Vue, options, {
         },
       }),
     ],
+  })
+
+  if (isClient && process.env.NODE_ENV === 'production') {
+    register(options.serviceWorkerPath, {
+      ready () {
+        console.log('Service worker is active.')
+      },
+      registered (registration) {
+        console.log('Service worker has been registered.')
+      },
+      cached (registration) {
+        console.log('Content has been cached for offline use.')
+      },
+      updatefound (registration) {
+        console.log('New content is downloading.')
+      },
+      updated (registration) {
+        console.log('New content is available; please refresh.')
+      },
+      offline () {
+        console.log('No internet connection found. App is running in offline mode.')
+      },
+      error (error) {
+        console.error('Error during service worker registration:', error)
+      }
+    })
+  }
+
+  head.link.push({
+    rel: 'manifest',
+    href: '/manifest.json'
   })
 }
