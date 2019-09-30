d92cf1d2c5d3d0f11532e4e91979f0e7111f95eb
Loke <deleted@example.com>
2019-09-30 07:55:22 +0200

    rework

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,7 +1,6 @@
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import * as Cookies from 'js-cookie'
-import { register } from 'register-service-worker'
 
 export default function (Vue, options, {
   appOptions,
@@ -59,36 +58,5 @@ export default function (Vue, options, {
         },
       }),
     ],
-  })
-
-  if (isClient && process.env.NODE_ENV === 'production') {
-    register(options.serviceWorkerPath, {
-      ready () {
-        console.log('Service worker is active.')
-      },
-      registered (registration) {
-        console.log('Service worker has been registered.')
-      },
-      cached (registration) {
-        console.log('Content has been cached for offline use.')
-      },
-      updatefound (registration) {
-        console.log('New content is downloading.')
-      },
-      updated (registration) {
-        console.log('New content is available; please refresh.')
-      },
-      offline () {
-        console.log('No internet connection found. App is running in offline mode.')
-      },
-      error (error) {
-        console.error('Error during service worker registration:', error)
-      }
-    })
-  }
-
-  head.link.push({
-    rel: 'manifest',
-    href: '/manifest.json'
   })
 }
--- gridsome.config.js
+++ gridsome.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   siteName: 'TCG',
-  plugins: []
+  plugins: [
+    {
+      use: 'gridsome-plugin-pwa',
+      options: {
+          title: 'Gridsome',
+          startUrl: '/',
+          display: 'standalone',
+          statusBarStyle: 'default',
+          manifestPath: 'manifest.json',
+          serviceWorkerPath: 'service-worker.js',
+          shortName: 'Gridsome',
+          themeColor: '#cb2d3e',
+          backgroundColor: '#232323',
+          icon: 'favicon.png'
+      }
+    }
+  ]
 }
