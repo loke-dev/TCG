1dc3bdbad5d90cdf02278881271d4fa03e1be04e
Loke <deleted@example.com>
2022-07-05 22:37:07 +0200

    feat: add PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,24 @@
 import { defineNuxtConfig } from 'nuxt'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   typescript: {
     shim: false
   },
-  vite: {
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        devOptions: {
-          enabled: true
-        }
-      })
-    ]
+  modules: [
+    '@kevinmarrec/nuxt-pwa'
+  ],
+  pwa: {
+    manifest: {
+      name: 'Team Color Generator',
+      short_name: 'TCG',
+      description: 'Generate team colors for playstation games.',
+      background_color: '#232323',
+      theme_color: '#cb2d3e',
+      lang: 'en',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+    }
   }
 })
